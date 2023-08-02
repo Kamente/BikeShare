d2e2d8a6989b6047b14d6bba581770d5eac0f075
isaacmogaka <isaacmogaka2016@gmail.com>
2023-08-03 00:18:00 +0300

    Created my ft-user-profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import UserList from './UserList';
+import UserProfile from './UserProfile';
 
 function App() {
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const handleUserClick = user => {
+    setSelectedUser(user);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!selectedUser ? (
+        <UserList handleUserClick={handleUserClick} />
+      ) : (
+        <UserProfile user={selectedUser} />
+      )}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
