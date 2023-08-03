d49fa95f0f421bdc8eac20386be8ca829a3bb70f
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-03 11:52:32 +0300

    Created authentication page

---
+++ src/Authenticationpage.js
@@ -0,0 +1,24 @@
+// AuthPage.js
+import React, { useState } from "react";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+function AuthenticationPage() {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const togglePage = () => {
+    setIsSignIn(!isSignIn);
+  };
+
+  return (
+    <div>
+      {isSignIn ? <Signin /> : <Signup />}
+      <button onClick={togglePage}>
+        {isSignIn ? "Don't have an account? Create account" : "Already have an account? Log in"}
+      </button>
+    </div>
+    
+  );
+}
+
+export default AuthenticationPage;
