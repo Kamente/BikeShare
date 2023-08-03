661aaa94dc2b90ce4ef9e2ea320909cd8b303f35
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-03 11:53:55 +0300

    styled the sign in and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthenticationPage from './Authenticationpage';
 
 function App() {
   return (
     <div className="App">
-     
+      <AuthenticationPage/>
     </div>
   );
 }
