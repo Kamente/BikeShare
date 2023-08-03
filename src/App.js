d867dac6f62f1bb5b7d5b38a91517f9be229538a
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-03 11:49:49 +0300

    deleted files and started afresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Userregistration from './Userregistration';
-import RegistrationForm from './RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm/>
-      <Userregistration/>
+     
     </div>
   );
 }
