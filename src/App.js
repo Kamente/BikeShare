4f4c8e437a8a7329a31e953c04b972622085503d
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-03 23:31:08 +0300

    deleted test files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthenticationPage from './Components/Authenticationpage';
-import Navigation from "./Components/Navigation"
-import Dashboard from './Components/Dashboard';
-import Preference from './Components/Preference';
 import Signin from './Components/Signin';
 
 
@@ -20,10 +17,7 @@ function App() {
     <div className="App">
       <h2>Bikeshare</h2>
       <BrowserRouter>
-      <Navigation/>
         <Routes>
-          <Route path='/dashboard' element ={<Dashboard/>}></Route>
-          <Route path='/preference' element ={<Preference/>}></Route>
           <Route path='/login' element= {<AuthenticationPage/>}></Route>
         </Routes>
       </BrowserRouter>
