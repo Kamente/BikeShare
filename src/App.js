e4cb4db18e5a6859e26f8d34fa7fa5749dc80a6c
Kamente <justinm48000@gmail.com>
2023-08-07 09:39:24 +0300

    homepage and booking form done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import "../src/components/Styles.css"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+// import Bikes from "./components/Bikes.js";
+// import Scooters from "./components/Scooters.js";
+import Vehicles from "./components/Vehicles.js";
+import HomePage from './components/Homepage';
+import About from './components/About';
+import BookingForm from './components/BookingForm'; 
+import BikesAndScooters from './components/BikesAndScooters';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booking/:type/:id" element={<BookingForm />} />
+          <Route path="/bikes" element={<BikesAndScooters />} />
+          <Route path="/scooters" element={<BikesAndScooters />} />
+          <Route path="/vehicles" element={<Vehicles />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
